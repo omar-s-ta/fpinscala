77988930d5d26bd8b7d2e17c9edb5a450fff8323
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2026-01-05 18:50:56 +0100

    impl: Option basic generic fn

--- src/main/scala/fpinscala/exercises/errorhandling/Option.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Option.scala
@@ -7,23 +7,30 @@ enum Option[+A]:
   case Some(get: A)
   case None
 
-  def map[B](f: A => B): Option[B] = ???
+  def map[B](f: A => B): Option[B] =
+    this match
+      case Some(get) => Some(f(get))
+      case None => None
 
-  def getOrElse[B >: A](default: => B): B = ???
+  def getOrElse[B >: A](default: => B): B =
+    this match
+      case Some(get) => get
+      case None => default
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f).getOrElse(None)
 
-  def orElse[B >: A](ob: => Option[B]): Option[B] = ???
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
+    map(Some(_)).getOrElse(ob)
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] =
+    flatMap(a => if f(a) then Some(a) else None)
 
 object Option:
 
   def failingFn(i: Int): Int =
-    val y: Int =
-      throw new Exception(
-        "fail!"
-      ) // `val y: Int = ...` declares `y` as having type `Int`, and sets it equal to the right hand side of the `=`.
+    val y: Int = // `val y: Int = ...` declares `y` as having type `Int`, and sets it equal to the right hand side of the `=`.
+      throw new Exception("fail!")
     try
       val x = 42 + 5
       x + y
@@ -34,8 +41,8 @@ object Option:
   def failingFn2(i: Int): Int =
     try
       val x = 42 + 5
-      x + ((throw new Exception(
-        "fail!")): Int) // A thrown Exception can be given any type; here we're annotating it with the type `Int`
+      // A thrown Exception can be given any type; here we're annotating it with the type `Int`
+      x + ((throw new Exception("fail!")): Int)
     catch case e: Exception => 43
 
   def mean(xs: Seq[Double]): Option[Double] =
